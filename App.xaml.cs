e5fe26fda29587c9d408444f2b6377b8ec12479f
Ariel Ramos <aram.cardenas84@hotmail.com>
2024-01-24 07:57:50 -0500

    Agregar archivos de proyecto.

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Ramos_ExProgreso33;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Ramos_ExProgreso33;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}

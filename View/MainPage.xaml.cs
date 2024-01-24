fcadeee2b5de01acebab30325955d65736a85a13
Ariel Ramos <aram.cardenas84@hotmail.com>
2024-01-24 08:58:52 -0500

    Implementada funcionalidad basica para consulta de frutas.

--- View/MainPage.xaml.cs
+++ View/MainPage.xaml.cs
@@ -5,17 +5,40 @@ namespace Ramos_ExProgreso33.View;
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
+    public int Index { get; set; } = 0;
 
 	public MainPage()
 	{
 		InitializeComponent();
+        //coleccion.ItemsSource = strings;
+        datos.Text = "\n";
 	}
 
-	private async void OnCounterClicked(object sender, EventArgs e)
+	private async void BotonClicked(object sender, EventArgs a) 
 	{
-		//CounterBtn.Text = await GetFruta();
-	}
+        string fruta = await GetFruta();
+        datos.Text = datos.Text + fruta;
+    }
 	
+    
+    public async Task<string> GetFruta()
+    {
+        string result = "nada";
+        string url = "https://www.fruityvice.com/api/fruit/all";
+        using (var httpclient = new HttpClient())
+        {
+            var response = await httpclient.GetAsync(url);
+            if (response.IsSuccessStatusCode)
+            {
+                var json = await response.Content.ReadAsStringAsync();
+                var frutas = JsonConvert.DeserializeObject<Fruta.Class1[]>(json);
+                var fruta = frutas[Index];
+                result = "\nNombre: " + fruta.name + "\nFamilia: " + fruta.family + "\nAzúcar: " + fruta.nutritions.sugar;
+            }
+        }
+        Index++;
+        return result;
+    }
+
 }
 
---
+++ ViewModel/ARMainPageViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ramos_ExProgreso33.ViewModel
+{
+    public class ARMainPageViewModel
+    {
+        
+    }
+}
